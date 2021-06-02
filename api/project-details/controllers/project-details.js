11cc3a55ad4ae0580b7ff7b5478e3e719726c950
Thinn <thinthinhlaing38@gmail.com>
2021-06-02 13:02:59 +0800

    overwrite controller

--- api/project-details/controllers/project-details.js
+++ api/project-details/controllers/project-details.js
@@ -1,8 +1,18 @@
-'use strict';
-
+"use strict";
+const { sanitizeEntity } = require("strapi-utils");
 /**
  * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#core-controllers)
  * to customize this controller
  */
 
-module.exports = {};
+module.exports = {
+  async create(ctx) {
+    let projectDetails = ctx.request.body;
+    projectDetails.user = ctx.state.user.id;
+
+    let entity = await strapi.services["project-details"].create(
+      projectDetails
+    );
+    return sanitizeEntity(entity, { model: strapi.models["project-details"] });
+  },
+};
